8ec40bff077a5c1df10ed3f052c9adc687ceb014
dongling.wen <dongling.wen@corp.to8to.com>
2023-04-27 14:45:14 +0800

    初始化

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+console.log("1111 import.meta.env", import.meta.env);
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,25 @@
 import { defineConfig, loadEnv } from "vite";
 import process from "process";
+import _ from "lodash";
 import vue from "@vitejs/plugin-vue";
-console.log("1111 import.meta.env.PORT ", import.meta);
 // https://vitejs.dev/config/
 
-// 第二步 根据不同环境 去读取 不同的 对象  进行返回
+// vite.config.js中访问env文件
 const envResolve = (mode) => {
+  // process.cwd()获取nodejs进程当前工作目录
   return loadEnv(mode, process.cwd());
 };
 
-// // 第三步 传 要读取的 参数 key
-// const getEnv = function (env) {
-//   //           我这里采用了lodash 读取 环境名
-//   return envResolve(_.last(process.argv))[env];
-// };
-// // 第四步 使用
-// getEnv("VITE_APP_MODULE");
+const getEnv = function (env) {
+  // process.argv 属性返回一个数组，这个数组包含了启动Node.js进程时的命令行参数
+  // last()返回数组最后一个元素
+  return envResolve(_.last(process.argv))[env];
+};
+
 export default defineConfig({
   plugins: [vue()],
-  // server: {
-  //   port: import.meta.env.PORT || 8081,
-  //   host: import.meta.env.HOST,
-  // },
+  server: {
+    port: Number(getEnv("VITE_PORT")) || 8081,
+    // host: import.meta.env.HOST,
+  },
 });
