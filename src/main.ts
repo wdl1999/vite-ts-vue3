a8bdb410cc5d01a05b2e4646228c13fcfa1a82b3
dongling.wen <dongling.wen@corp.to8to.com>
2024-03-06 16:42:29 +0800

    puppteer不能在es6使用-删除

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import "./style.css";
 import App from "./App.vue";
 import "@/styles/base.scss";
 import routes from "@/routes/index";
-// import { puppeteer } from 'puppeteer';
 import { createPinia } from "pinia";
 import { createRouter, createWebHashHistory } from "vue-router";
 // 使用import.meta.env可以获取默认的环境变量以及.env中以VITE_开头的环境变量
@@ -12,7 +11,7 @@ console.log("1111 __APP_VERSION__", __APP_VERSION__, typeof __APP_VERSION__);
 
 const router = createRouter({
     history: createWebHashHistory(),
-    routes: [{ path: "/", redirect: "puppeteer" }, ...routes],
+    routes: [{ path: "/", redirect: "/helloWorld" }, ...routes],
 });
 
 // 创建一个pinia 实例（根store）并将其传递给应用
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,6 @@ import type { RouteRecordRaw } from "vue-router";
 
 const appRoutes: RouteRecordRaw[] = [
     {
-        path: "/puppeteer",
-        name: "puppeteer",
-        component: () => import("@/components/puppeteer.vue"),
-
-    },
-    {
         path: "/helloWorld",
         name: "helloWorld",
         component: () => import("@/components/HelloWorld.vue"),
